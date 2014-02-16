9eca5fb0858dbef08b4fe456d9ee83af0b51afea
Adam Tomat <hello@adam-tomat.co.uk>
2014-02-16 14:29:46 +0000

    First Commit

---
+++ web/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>interAct</title>
+		<link rel="stylesheet" type="text/css" href="/css/style.css" media="screen, handheld" />
+		<link rel="stylesheet" type="text/css" href="/css/enhanced.css" media="screen and (min-width: 40.5em)" />
+		<script src="bower_components/modernizr/modernizr.js"></script>
+	</head>
+	<body>
+		
+		<div class="slab slab--light-green">
+			<div class="inner">
+				<header role="banner" class="banner">
+					<h1 class="banner__title">
+						<span class="banner__word banner__word--dark">inter</span>
+						<span class="banner__word banner__word--light">Act</span>
+					</h1>
+				</header>
+			</div>
+		</div>
+
+		<div class="slab slab--blue">
+			<div class="inner">
+				<div class="intro">
+					<p class="intro__paragraph">We aim to deliver bespoke packages for Actors within the performance industry.</p>
+					<p class="intro__paragraph">We are looking for people to collaborate on this project &amp; are particularly looking for experienced Graphics Designers in multiple disciplines (print, logo &amp; web design).</p>
+					<p class="intro__paragraph">For more information regarding our project please get in touch using the form below.</p>
+				</div>
+			</div>
+			<div class="inner">
+				<div class="form-wrapper">
+					<form class="form">
+						<div class="row collapse">
+							<div class="large-4 small-12 columns">
+								<div class="field-container">
+									<label for="name">Name:</label>
+									<input type="text" id="name" class="name" placeholder="Your name..." />
+								</div>
+							</div>
+							<div class="large-4 small-12 columns">
+								<div class="field-container">
+									<label for="email">Email Address:</label>
+									<input type="text" id="email" class="email" placeholder="you@email.com" />
+								</div>
+							</div>
+							<div class="large-4 small-12 columns">
+								<div class="field-container">
+									<label for="portfolio" class="optional">Portfolio:</label>
+									<input type="text" id="portfolio" class="portfolio" placeholder="www.mywork.co.uk" />
+								</div>
+							</div>
+						</div>
+						<div class="submit-wrapper">
+							<input type="submit" id="submit" name="submit" value="Register my interest" class="large radius success button" />
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<script src="bower_components/jquery/jquery.js"></script>
+		<script src="bower_components/foundation/js/foundation.min.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+</html>
