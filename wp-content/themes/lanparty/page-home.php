7c4d1939dac0a93ca92e4de17f552e36bc395d23
Adam Tomat <hello@adam-tomat.co.uk>
2014-05-11 13:08:27 +0100

    Added copy and styled up.

--- wp-content/themes/lanparty/page-home.php
+++ wp-content/themes/lanparty/page-home.php
@@ -11,6 +11,40 @@ Template Name: Home
 		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 			<?php the_content(); ?>
 		<?php endwhile; ?>
+
+		<h2>Who Runs the Event?</h2>
+		<div class="row">
+			<div class="large-6 columns small-6">
+				<div class="organiser">
+					<div class="organiser__image">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/lanparty/images/biles.jpeg" />
+					</div>
+					<span class="organiser__name">Oliver Biles</span>
+					<a href="http://twitter.com/OliverBiles" class="organiser__twitter">@OliverBiles</a>
+				</div>
+			</div>
+			<div class="large-6 columns small-6">
+				<div class="organiser">
+					<div class="organiser__image">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/lanparty/images/henry.png" />
+					</div>
+					<span class="organiser__name">Henry Garle</span>
+					<!-- <a href="http://twitter.com/henrygarle" class="organiser__twitter">@henrygarle</a> -->
+
+				</div>
+				
+			</div>
+		</div>
+
+		<h2>Previous attendees</h2>
+		<div class="attendees">
+			<?php $names = array('10shinhan', 'Biles', 'BoBaCk', 'Jordeh', 'Nut315', 'Tman', 'George'); 
+			sort($names);
+
+			foreach ($names as $name) : ?>
+				<div class="attendee"><span class="attendee__name"><?php echo $name; ?></span></div>
+			<?php endforeach; ?>
+		</div>
 	</div>
 </div>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
--- wp-content/themes/lanparty/parts/shared/html-footer.php
+++ wp-content/themes/lanparty/parts/shared/html-footer.php
@@ -1,10 +1,9 @@
 	<?php /*
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/interact/components/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/lanparty/components/jquery/dist/jquery.min.js"><\/script>')</script>
 
-	<script src="<?php bloginfo('template_directory'); ?>/interact/components/foundation/js/foundation.min.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/interact/components/modernizr/modernizr.js"></script>
-	/**/?>
+	<script src="<?php bloginfo('template_directory'); ?>/lanparty/components/foundation/js/foundation.min.js"></script>/***/ ?>
+	<script src="<?php bloginfo('template_directory'); ?>/lanparty/components/modernizr/modernizr.js"></script>
 	<?php wp_footer(); ?>
 	</body>
 </html>
